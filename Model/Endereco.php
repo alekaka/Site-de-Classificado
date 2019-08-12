f69e69a481e3b39b7846aeefd47e754e0b0a4c1b
Thiago Alexander <athiago884@gmail.com>
2019-08-12 12:17:16 -0300

    Criando a classe endereco e seus encapsulamentos.

---
+++ Model/Endereco.php
@@ -0,0 +1,103 @@
+<?php 
+
+class Endereco {
+    private $cod;
+    private $rua;
+    private $numero;
+    private $bairro;
+    private $cidade;
+    private $estado;
+    private $complemento;
+    private $cep;
+    private $usuario;
+    
+    public function setCod($cod)
+    {
+        $this->cod = $cod;
+    }
+    
+    public function getCod()
+    {
+        return $this->cod;
+    }
+
+    public function setRua($rua)
+    {
+        $this->rua = $rua;
+    }
+    
+    public function getRua()
+    {
+        return $this->rua;
+    }
+
+    public function setNumero($numero)
+    {
+        $this->numero = $numero;
+    }
+    
+    public function getNumero()
+    {
+        return $this->numero;
+    }
+
+    public function setBairro($bairro)
+    {
+        $this->bairro = $bairro;
+    }
+    
+    public function getBairro()
+    {
+        return $this->bairro;
+    }
+
+    public function setCidade($cidade)
+    {
+        $this->cidade = $cidade;
+    }
+    
+    public function getCidade()
+    {
+        return $this->cidade;
+    }
+
+    public function setEstado($estado)
+    {
+        $this->estado = $estado;
+    }
+    
+    public function getEstado()
+    {
+        return $this->estado;
+    }
+
+    public function setComplemento($complemento)
+    {
+        $this->complemento = $complemento;
+    }
+    
+    public function getComplemento()
+    {
+        return $this->complemento;
+    }
+
+    public function setCep($cep)
+    {
+        $this->cep = $cep;
+    }
+    
+    public function getCep()
+    {
+        return $this->cep;
+    }
+
+    public function setUsuario($usuario)
+    {
+        $this->usuario = $usuario;
+    }
+    
+    public function getUsuario()
+    {
+        return $this->usuario;
+    }
+}
