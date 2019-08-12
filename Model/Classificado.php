4722002e63bcf2fd37870b28c3ecac000ea4a036
Thiago Alexander <athiago884@gmail.com>
2019-08-12 15:33:53 -0300

    Criando a classe classificado e seus encapsulamentos.

---
+++ Model/Classificado.php
@@ -0,0 +1,115 @@
+<?php 
+
+class Classificado {
+
+    private $cod;
+    private $nome;
+    private $descricao;
+    private $tipo;
+    private $valor;
+    private $data;
+    private $status;
+    private $perfil;
+    private $categoria;
+    private $usuario;
+    
+    public function setCod($cod)
+    {
+        $this->cod = $cod;
+    }
+    
+    public function getCod()
+    {
+        return $this->cod;
+    }
+
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+    }
+    
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    public function setDescricao($descricao)
+    {
+        $this->descricao = $descricao;
+    }
+    
+    public function getDescricao()
+    {
+        return $this->descricao;
+    }
+
+    public function setTipo($tipo)
+    {
+        $this->tipo = $tipo;
+    }
+    
+    public function getTipo()
+    {
+        return $this->tipo;
+    }
+
+    public function setValor($valor)
+    {
+        $this->valor = $valor;
+    }
+    
+    public function getValor()
+    {
+        return $this->valor;
+    }
+
+    public function setData($data)
+    {
+        $this->data = $data;
+    }
+    
+    public function getData()
+    {
+        return $this->data;
+    }
+
+    public function setStatus($status)
+    {
+        $this->status = $status;
+    }
+    
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    public function setPerfil($perfil)
+    {
+        $this->perfil = $perfil;
+    }
+    
+    public function getPerfil()
+    {
+        return $this->perfil;
+    }
+
+    public function setCategoria($categoria)
+    {
+        $this->categoria = $categoria;
+    }
+    
+    public function getCategoria()
+    {
+        return $this->categoria;
+    }
+
+    public function setUsuario($usuario)
+    {
+        $this->usuario = $usuario;
+    }
+    
+    public function getUsuario()
+    {
+        return $this->usuario;
+    }
+}
