511650e251123f861d8932234e85133f4061e83e
Thiago Alexander <athiago884@gmail.com>
2019-08-13 11:12:39 -0300

    Importando o bootstrap.min.css, jquery-3.1.1.min e o bootstrap.min.js.

--- Admin-bc/painel.php
+++ Admin-bc/painel.php
@@ -3,6 +3,9 @@
     <head>
         <title>Brasil Classificados - Painel</title>
         <meta charset="UTF-8"></meta>
+        <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <script src="../js/jquery-3.1.1.min.js" type="text/javascript"></script>
+        <script src="../bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
     </head>
     <body>
         
