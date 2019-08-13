bf158e9e92bffc44a2cf3365573c83aff4641843
Thiago Alexander <athiago884@gmail.com>
2019-08-13 11:22:37 -0300

    Adicionando icon.

--- Admin-bc/painel.php
+++ Admin-bc/painel.php
@@ -7,6 +7,7 @@
         <script src="../js/jquery-3.1.1.min.js" type="text/javascript"></script>
         <script src="../bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
         <link href="css/style.css" rel="stylesheet" type="text/css"/>
+        <link rel="shortout icon" href="../img/favicon.ico" />
     </head>
     <body>
         
