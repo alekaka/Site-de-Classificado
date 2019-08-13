147f222a2493c1b0378d91254c7eee755e5fa069
Thiago Alexander <athiago884@gmail.com>
2019-08-13 11:19:31 -0300

    Criando a folhar de estilo do painel e importando.

--- Admin-bc/painel.php
+++ Admin-bc/painel.php
@@ -6,6 +6,7 @@
         <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
         <script src="../js/jquery-3.1.1.min.js" type="text/javascript"></script>
         <script src="../bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+        <link href="css/style.css" rel="stylesheet" type="text/css"/>
     </head>
     <body>
         
